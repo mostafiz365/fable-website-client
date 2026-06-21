32b187ff2779b27c193393bbf3db895da1c9b14c
mostafiz365 <rahmanmostafiz365@gmail.com>
2026-06-21 18:28:58 +0600

    create admin all-ebooks components and load all books get api

--- src/app/dashboard/admin/all-ebooks/page.jsx
+++ src/app/dashboard/admin/all-ebooks/page.jsx
@@ -1,9 +1,11 @@
+import { getBooks } from '@/lib/api/books';
 import React from 'react';
 
-const AllEbooksPage = () => {
+const AllEbooksPage = async() => {
+    const books = await getBooks();
     return (
         <div>
-            <h2>Here out All Ebooks</h2>
+            <h2>Here out All Ebooks : {books.length}</h2>
         </div>
     );
 };
--- src/lib/api/books.js
+++ src/lib/api/books.js
@@ -1,9 +1,9 @@
 import { serverFetch } from "../core/server";
 
 
-// export const getBooks = async() =>{
-//     return serverFetch('/api/books');
-// }
+export const getBooks = async() =>{
+    return serverFetch('/api/all-books');
+}
 
 // এই ফাংশনটি কল করলেই এখন ব্যাকএন্ড থেকে শুধুমাত্র পাবলিশড বইগুলো চলে আসবে
 export const getPublishedBooks = async () => {
